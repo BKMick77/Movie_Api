e14eb7136be38d5985bf6aa337ccedaa1b35c166
Mike Hyler <mhyler@zoho.com>
2025-06-27 01:25:42 -0400

    http logging

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-  level: 'info',
+  level: 'http',
   format: format.combine(format.timestamp(), format.simple()),
   transports: [
     new transports.Console(),
