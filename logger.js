d14390b4f15a710bd095c2a868785020ca7f1765
Mike Hyler <mhyler@zoho.com>
2025-06-27 09:36:53 -0400

    updated logging format

--- logger.js
+++ logger.js
@@ -1,8 +1,15 @@
 const { createLogger, format, transports } = require('winston');
 
+const logFormat = format.printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
 const logger = createLogger({
   level: 'http',
-  format: format.combine(format.timestamp(), format.simple()),
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
   transports: [
     new transports.Console(),
     new transports.File({ filename: 'logfile.log' }),
